b88074b7028bdfafa271f626e9686d405c13939a
IMike17 <iovinomic@gmail.com>
2019-01-14 19:23:00 +0100

    Adding temp file to check configuration

---
+++ Neatworking/Temp.swift
@@ -0,0 +1,15 @@
+//
+// Temp.swift
+// Neatworking
+//
+// Created by Michele Iovino on 14/01/2019.
+//
+// Created for Neatworking in 2019
+// Using Swift 4.0
+// Running on macOS 10.14
+//
+// Copyright (C) 2019 Michele Iovino <iovinomic@gmail.com>. All rights reserved.
+//
+
+
+import Foundation
